7a10b749dd5da49e2b4685d3b61452937b1c886c
zablon <zablonoigo51@gmail.com>
2026-08-20 21:20:52 +0300

    habit track application

---
+++ src/main/java/org/habit/TrackApplication.java
@@ -0,0 +1,13 @@
+package org.habit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackApplication.class, args);
+	}
+
+}
