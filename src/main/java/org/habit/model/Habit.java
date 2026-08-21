17afbd9e5d656c781b3dc392b1c27b333b7fefb3
zablon <zablonoigo51@gmail.com>
2026-08-21 08:50:49 +0300

    fix old hibernate uuid

--- src/main/java/org/habit/model/Habit.java
+++ src/main/java/org/habit/model/Habit.java
@@ -1,15 +1,20 @@
 package org.habit.model;
 
-import jakarta.persistence.*;
+import java.time.LocalDate;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.GenericGenerator;
-
-import java.time.LocalDate;
-import java.util.UUID;
 
 @Entity
 @Builder(toBuilder = true)
@@ -21,8 +26,7 @@ import java.util.UUID;
 public class Habit {
 
     @Id
-    @GeneratedValue(generator = "uuid2")
-    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @GeneratedValue
     private UUID id;
 
     @Column(nullable = false, unique = true)
@@ -49,5 +53,4 @@ public class Habit {
     public int hashCode() {
         return id != null ? id.hashCode() : 0;
     }
-
 }
