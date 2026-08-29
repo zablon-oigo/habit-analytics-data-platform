e905339023e129e429429dd62aa105543833a617
zablon <zablonoigo51@gmail.com>
2026-08-29 21:05:41 +0300

    kafka streams config

---
+++ src/main/java/org/habit/analytics/KafkaStreamsConfig.java
@@ -0,0 +1,10 @@
+package org.habit.analytics;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
+
+@Configuration
+@EnableKafkaStreams
+public class KafkaStreamsConfig {
+
+}
